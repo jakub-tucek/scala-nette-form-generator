43aaa7c2aab6f99a4bdbdcae9763432418ceb206
Jakub Tucek <jakub.tucek@gmail.com>
2018-03-12 20:41:32 +0100

    Fix missing import in home controller

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,5 +1,10 @@
 package controllers
 
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
