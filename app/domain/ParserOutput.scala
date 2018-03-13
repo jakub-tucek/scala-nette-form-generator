fdcd538f1bde475d4e0aeb5b7b25a88f8ba6a5de
Jakub Tucek <jakub.tucek@gmail.com>
2018-03-13 15:31:10 +0100

    Add table parser

--- app/domain/ParserOutput.scala
+++ app/domain/ParserOutput.scala
@@ -4,7 +4,31 @@ package domain
   *
   * @author Jakub Tucek
   */
-class ParserOutput {
+class ParserOutput(val tables: List[Table]) {
+
+}
+
+class Table(val name: String, val cols: List[Columns]) {
+
+  def this(firstName: String) {
+    this(firstName, "", 0)
+    println("\nNo last name or age given.")
+  }
+}
+
+class Columns(val name: String, val options: List[ColumnOption]) {
+
+}
+
+abstract class ColumnOption
+
+case class ColumnDefaultValue(sender: String, title: String, body: String) extends ColumnOption
+
+case class ColumnRequired(required: Boolean) extends ColumnOption
+
+case class ColumnMaxLength(length: Integer) extends ColumnOption
+
+case class ColumnEnumTypes(types: List[String]) extends ColumnOption
 
 }
 
