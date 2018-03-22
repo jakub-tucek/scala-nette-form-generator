9c5b71c9f9d1b3c9fc9c957a0743efb5f4edd536
Jakub Tucek <jakub.tucek@gmail.com>
2018-03-22 23:30:19 +0100

    Add timer to navigation

--- web/src/main/scala/components/NavComponent.scala
+++ web/src/main/scala/components/NavComponent.scala
@@ -2,10 +2,11 @@ package components
 
 import java.time.LocalDateTime
 
-import japgolly.scalajs.react.ScalaComponent
 import japgolly.scalajs.react.vdom.html_<^._
-import org.scalajs.dom.experimental.intl.DateTimeFormatOptions
+import japgolly.scalajs.react.{BackendScope, Callback, ScalaComponent}
 import utils.ViewUtils
+
+import scala.scalajs.js
 
 /**
   *
@@ -13,23 +14,48 @@ import utils.ViewUtils
   */
 object NavComponent {
 
-  private val component = ScalaComponent.builder[Unit]("HelloMessage")
-    .render(_ => <.nav(
-      ^.cls := "navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0",
-      <.a(
-        ^.cls := "navbar-brand col-sm-3 col-md-2 mr-0",
-        ^.href := "#",
-        "Nette form generator"
-      ),
-      <.ul(
-        ^.cls := "navbar-nav px-3",
-        <.li(
-          ^.cls := "nav-item text-nowrap",
-          ViewUtils.formatDate(LocalDateTime.now)
-        )
-      )
-    )
-    ).build
+  private val component = ScalaComponent
+    .builder[Unit]("NavComponent")
+    .initialState(getCurrentDate)
+    .renderBackend[Backend]
+    .componentDidMount(_.backend.mounted())
+    .componentWillUnmount(_.backend.unmounted())
+    .build
 
   def apply() = component()
+
+  private def getCurrentDate: String = ViewUtils.formatDate(LocalDateTime.now)
+
+  class Backend($: BackendScope[Unit, String]) {
+    var interval: js.UndefOr[js.timers.SetIntervalHandle] = js.undefined
+
+    def mounted() = Callback {
+      interval = js.timers.setInterval(1000)(refreshTime())
+    }
+
+    def refreshTime() = $.setState(getCurrentDate).runNow()
+
+    def unmounted() = Callback {
+      interval foreach js.timers.clearInterval
+      interval = js.undefined
+    }
+
+    def render(state: String): VdomTag = {
+      <.nav(
+        ^.cls := "navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0",
+        <.a(
+          ^.cls := "navbar-brand col-sm-3 col-md-2 mr-0",
+          ^.href := "#",
+          "Nette form generator"
+        ),
+        <.ul(
+          ^.cls := "navbar-nav px-3",
+          <.li(
+            ^.cls := "nav-item text-nowrap",
+            state
+          )
+        )
+      )
+    }
+  }
 }
