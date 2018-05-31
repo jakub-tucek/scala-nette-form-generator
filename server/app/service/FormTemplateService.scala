0d5ccc79e7f6fe7fcfdefb896b7a63394b47858c
Jakub Tucek <jakub.tucek@gmail.com>
2018-05-31 21:18:04 +0200

    Add base php template generation

--- server/app/service/FormTemplateService.scala
+++ server/app/service/FormTemplateService.scala
@@ -5,6 +5,7 @@ import javax.inject.Singleton
 import shared.domain.{FormTemplate, FormTemplateContainer, LatteTemplateType, PhpTemplateType}
 
 /**
+  * Service responsible for creation of templates based on parsed data.
   *
   * @author Jakub Tucek
   */
@@ -12,13 +13,27 @@ import shared.domain.{FormTemplate, FormTemplateContainer, LatteTemplateType, Ph
 class FormTemplateService {
 
   def createLatteTemplates(tables: List[Table]): FormTemplateContainer = {
-    val templates = tables.map(t => FormTemplate(t.name, views.html.bootstrapForm.render(t).body))
+    // generate latte (bootstrap) tempates
+    val templates = tables map { t => FormTemplate(formatTableName(t.name), views.html.bootstrapForm.render(t).body) }
 
     FormTemplateContainer(templates, LatteTemplateType())
   }
 
   def createPhpTemplates(tables: List[Table]): FormTemplateContainer = {
-
-    FormTemplateContainer(List(), PhpTemplateType())
+    // generate php templates
+    val templates = tables map { t =>
+      val formattedName = formatTableName(t.name)
+      FormTemplate(formattedName, views.html.phpForm.render(t, formattedName).body)
+    }
+    FormTemplateContainer(templates, PhpTemplateType())
   }
+
+  /**
+    * Formats table name to CamelCase. Capitalizes each letter after '_' and removes delimiter.
+    * First letter is capitalized too.
+    *
+    * @param n original name
+    * @return formatted name
+    */
+  private def formatTableName(n: String): String = n.split('_').map(_.capitalize).mkString("").capitalize
 }
